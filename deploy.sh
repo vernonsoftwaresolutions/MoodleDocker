c76a7c082a85da0a37d9979193c44f2ab3e082ff
Andrew Larsen <andrewlarsen@Andrews-MacBook-Pro.local>
2017-05-17 21:37:35 -0500

    add docker deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+EB_BUCKET=$1
+VERSION=$2
+echo $EB_BUCKET
+echo $VERSION
+
+#docker file
+DOCKERRUN_FILE=$VERSION-Dockerrun.aws.json
+echo $DOCKERRUN_FILE
+
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
