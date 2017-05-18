da4947ad422cc2675cbdcd1db53d9c1f7cdce603
Andrew Larsen <andrewlarsen@Andrews-MacBook-Pro.local>
2017-05-17 21:49:03 -0500

    fix order

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,5 @@ echo $VERSION
 #docker file
 DOCKERRUN_FILE=$VERSION-Dockerrun.aws.json
 echo $DOCKERRUN_FILE
-
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
