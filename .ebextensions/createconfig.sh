ff4de62dca24c16329cbfa2e31f78390a8d059fc
Andrew Larsen <andrewlarsen@Andrews-MacBook-Pro.local>
2017-05-20 12:40:50 -0500

    update createconfig.sh

--- .ebextensions/createconfig.sh
+++ .ebextensions/createconfig.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
+echo "about to modify config.php with the following env variables $DBURL, $DBNAME, $DBUSER, password"
 sed "s/<DATABASEURL>/$DBURL/" < config.php > config.php
 sed "s/<DATABASENAME>/$DBNAME/" < config.php > config.php
 sed "s/<DATABASEUSER>/$DBUSER/" < config.php > config.php
 sed "s/<DATABASEPASSWORD>/$DBPASSWORD/" < config.php > config.php
-
+echo "modified config.php, moving config.php"
 mv config.php /
